8f6c86925638f92e1f7c429a4bc1544e5f0281ff
riyadhossain565 <hossainriyad565@gmail.com>
2024-12-06 02:43:42 +0600

    added functionalites to addEquipment page

--- src/Pages/AddEquipment.jsx
+++ src/Pages/AddEquipment.jsx
@@ -1,4 +1,78 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import Swal from "sweetalert2";
+
 const AddEquipment = () => {
+  const { user } = useContext(AuthContext);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/users/${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setUserData(data))
+        .catch((error) => {
+          console.log("Error data:", error);
+          setUserData(null);
+        });
+    }
+  }, [user]);
+
+  const handleAddItems = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const name = userData?.name;
+    const email = userData?.email;
+    const itemName = form.itemName.value;
+    const category = form.category.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const customization = form.customization.value;
+    const processingTime = form.processingTime.value;
+    const image = form.image.value;
+    const stockStatus = form.stockStatus.value;
+    const description = form.description.value;
+
+    const newEquipment = {
+      name,
+      email,
+      itemName,
+      category,
+      price,
+      rating,
+      customization,
+      processingTime,
+      image,
+      stockStatus,
+      description,
+    };
+
+    console.log(newEquipment);
+
+    // send data to the server
+    fetch("http://localhost:5000/equipment", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newEquipment),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "New Sports Equipment Added Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12">
       <div className="bg-white w-full max-w-4xl p-8 rounded-lg shadow-lg">
@@ -6,21 +80,17 @@ const AddEquipment = () => {
           Add New Equipment
         </h2>
 
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleAddItems}>
           {/* row-1 */}
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             {/* User Name (Read-Only) */}
             <div>
-              <label
-                className="block text-gray-700 font-semibold mb-2"
-                htmlFor="userName"
-              >
+              <label className="block text-gray-700 font-semibold mb-2">
                 User Name
               </label>
               <input
                 type="text"
-                id="userName"
-                value="John Doe"
+                value={userData?.name}
                 readOnly
                 className="w-full px-4 py-3 border-2 border-gray-300 bg-gray-100 rounded-lg cursor-not-allowed"
               />
@@ -28,16 +98,12 @@ const AddEquipment = () => {
 
             {/* User Email (Read-Only) */}
             <div>
-              <label
-                className="block text-gray-700 font-semibold mb-2"
-                htmlFor="userEmail"
-              >
+              <label className="block text-gray-700 font-semibold mb-2">
                 User Email
               </label>
               <input
                 type="email"
-                id="userEmail"
-                value="user@example.com"
+                value={userData?.email}
                 readOnly
                 className="w-full px-4 py-3 border-2 border-gray-300 bg-gray-100 rounded-lg cursor-not-allowed"
               />
@@ -48,15 +114,12 @@ const AddEquipment = () => {
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             {/* Item Name */}
             <div>
-              <label
-                className="block text-gray-700 font-semibold mb-2"
-                htmlFor="itemName"
-              >
+              <label className="block text-gray-700 font-semibold mb-2">
                 Item Name
               </label>
               <input
                 type="text"
-                id="itemName"
+                name="itemName"
                 placeholder="Enter item name"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
@@ -72,7 +135,7 @@ const AddEquipment = () => {
               </label>
               <input
                 type="text"
-                id="categoryName"
+                name="category"
                 placeholder="Enter category name"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
@@ -91,6 +154,7 @@ const AddEquipment = () => {
               </label>
               <input
                 type="text"
+                name="price"
                 placeholder="Enter price"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
@@ -106,6 +170,7 @@ const AddEquipment = () => {
               </label>
               <input
                 type="text"
+                name="rating"
                 placeholder="Enter rating"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
@@ -125,7 +190,7 @@ const AddEquipment = () => {
               </label>
               <input
                 type="text"
-                id="customization"
+                name="customization"
                 placeholder="Enter customization details"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
@@ -141,7 +206,7 @@ const AddEquipment = () => {
               </label>
               <input
                 type="text"
-                id="processingTime"
+                name="processingTime"
                 placeholder="Enter processing time"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
@@ -153,14 +218,12 @@ const AddEquipment = () => {
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
             {/* Image Upload */}
             <div>
-              <label
-                className="block text-gray-700 font-semibold mb-2"
-                htmlFor="image"
-              >
+              <label className="block text-gray-700 font-semibold mb-2">
                 Photo Url
               </label>
               <input
                 type="text"
+                name="image"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
                 placeholder="Enter Photo Url"
               />
@@ -176,6 +239,7 @@ const AddEquipment = () => {
               </label>
               <input
                 type="text"
+                name="stockStatus"
                 placeholder="Enter available quantity"
                 className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
               />
@@ -191,7 +255,7 @@ const AddEquipment = () => {
               Description
             </label>
             <textarea
-              id="description"
+              name="description"
               placeholder="Enter item description"
               rows="4"
               className="w-full px-4 py-3 border-2 border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
